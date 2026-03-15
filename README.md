df2828b7048f37b96da9bfc4a043733bcc446147
Piyusha <piyushamasurkar4@gmail.com>
2026-03-16 00:29:18 +0530

    new changes

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-## Robot Package Template
-
-This is a GitHub template. You can make your own copy by clicking the green "Use this template" button.
-
-It is recommended that you keep the repo/package name the same, but if you do change it, ensure you do a "Find all" using your IDE (or the built-in GitHub IDE by hitting the `.` key) and rename all instances of `my_bot` to whatever your project's name is.
-
-Note that each directory currently has at least one file in it to ensure that git tracks the files (and, consequently, that a fresh clone has direcctories present for CMake to find). These example files can be removed if required (and the directories can be removed if `CMakeLists.txt` is adjusted accordingly).
-
+This is beetle by TEAM MARUTI
 
 #launch
-ros2 launch my_bot launch_sim.launch.py
+ros2 launch beetle launch_sim.launch.py
 
 #to publish joint_states
 ros2 run joint_state_publisher joint_state_publisher
@@ -16,4 +9,8 @@ ros2 run joint_state_publisher joint_state_publisher
 #to launch slam toolbox
 ros2 launch slam_toolbox online_async_launch.py params_file:=./src/beetle/config/mapper_params_online_async.yaml use_sim_time:=true
 
+#to save map 
+ros2 run nav2_map_server map_saver_cli -f my_map
 
+#to load rviz as it is
+rviz2 -d src/beetle/config/main.rviz 
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,67 +1,114 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
-    package_name = 'my_bot'
+    package_name = "beetle"
+    pkg_share = get_package_share_directory(package_name)
 
-    # WORLD PATH
     world_file_path = os.path.join(
-        get_package_share_directory(package_name),
-        'worlds',
-        'obstacle_world.world'
+        pkg_share,
+        "worlds",
+        "obstacle_world.world"
     )
 
+    map_file = "/home/piyusha/beetle_ws/saved.yaml"
+
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory(package_name),
-                'launch',
-                'rsp.launch.py'
-            )
+            os.path.join(pkg_share, "launch", "rsp.launch.py")
         ),
-        launch_arguments={'use_sim_time': 'true'}.items()
+        launch_arguments={"use_sim_time": "true"}.items(),
     )
 
-    # GAZEBO WITH CUSTOM WORLD
+    # Gazebo Launch
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory('gazebo_ros'),
-                'launch',
-                'gazebo.launch.py'
+                get_package_share_directory("gazebo_ros"),
+                "launch",
+                "gazebo.launch.py",
             )
         ),
-        launch_arguments={'world': world_file_path}.items()
+        launch_arguments={"world": world_file_path}.items(),
     )
 
+    # Spawn Robot
     spawn_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+        package="gazebo_ros",
+        executable="spawn_entity.py",
         arguments=[
-            '-topic', 'robot_description',
-            '-entity', 'my_bot'
+            "-topic",
+            "robot_description",
+            "-entity",
+            "beetle",
         ],
-        output='screen'
+        output="screen",
     )
 
+    # Joint State Publisher
     joint_state_publisher = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
-        parameters=[{"use_sim_time": True}]
+        parameters=[{"use_sim_time": True}],
     )
 
-    return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity,
-        joint_state_publisher
-    ])
+    # Map Server
+    map_server = Node(
+        package="nav2_map_server",
+        executable="map_server",
+        name="map_server",
+        output="screen",
+        parameters=[
+            {"yaml_filename": map_file},
+            {"use_sim_time": True},
+        ],
+    )
+
+    # AMCL Localization
+    amcl = Node(
+        package="nav2_amcl",
+        executable="amcl",
+        name="amcl",
+        output="screen",
+        parameters=[
+            os.path.join(
+                pkg_share,
+                "config",
+                "amcl.yaml"
+            ),
+            {"use_sim_time": True},
+        ],
+    )
+
+    # Lifecycle Manager
+    lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        parameters=[
+            {"use_sim_time": True},
+            {"autostart": True},
+            {"node_names": ["map_server", "amcl"]},
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            rsp,
+            gazebo,
+            spawn_entity,
+            joint_state_publisher,
+            map_server,
+            amcl,
+            lifecycle_manager,
+        ]
+    )
